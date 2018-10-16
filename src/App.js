201c77bd5edf960790f9ebef8fe3a9d6488dff8e
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-16 13:42:39 -0600

    Rework App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      stars: ''
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(process.env.REACT_APP_DATABASE_API_URL)
+      const data = await response.json()
+      console.log(data)
+      console.log('you are on heroku heney')
+    }
+    catch(error) {
+      return error.message
+    }
+  }
+
   render() {
     return (
       <div className="App">
