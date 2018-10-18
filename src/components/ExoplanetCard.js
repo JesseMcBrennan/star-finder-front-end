6df95702431dbb009555567369c8ca7314f8d6fc
Lee Graham <graham.la3@gmail.com>
2018-10-17 21:03:29 -0600

    Add edit text function props to card component

--- src/components/ExoplanetCard.js
+++ src/components/ExoplanetCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ExoplanetCard = ({name, mass, id, removePlanet}) => {
+const ExoplanetCard = ({name, mass, id, removePlanet, editPlanet}) => {
   return(
     <div className="Card">
-      <h3>{name}</h3>
+      <h3 contenteditable="true" onClick={() => editPlanet(id)}>{name}</h3>
       <p>{mass}</p>
       <button onClick={() => removePlanet(id)}>Delete</button>
     </div>
